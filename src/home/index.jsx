6dbdbc2e1a5398d7caaf6ae96f2125385bf5ad80
devashish gupta <devashishgupta20102000@gmail.com>
2024-04-10 18:48:56 +0530

    add

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -8,20 +8,30 @@ const Home = () => {
   return (
    <>
    
-   <Navbar/>
-   <div className='top-4 fixed left-8'>
-   <div className=' font-semibold'>Securepacks</div>
+   <div className='flex flex-col flex-auto'>
+   <div className='flex justify-between items-center pl-8'>
+   <div className=' font-semibold '>Securepacks</div>
+    <Navbar/>
    </div>
-   <div className='flex justify-end  mt-20 mr-40 gap-52'>
-    <div className='w-[60%] ml-[10%] text-center'>
-    <h1 className="font-bold text-3xl mt-8 bg-gradient-to-r from-purple-600 to-pink-500 text-transparent bg-clip-text">Minimum Complexity</h1>
-      <h1 className='font-bold text-3xl ml-60 bg-gradient-to-r from-purple-600 to-pink-500 text-transparent bg-clip-text'> Maximum Security</h1>
-      <p>Your custom app, meticulously tailored to your language preference, fortified with robust security measures, and primed for seamless deployment in the cloud.</p>
+  
+   <div className='flex justify-center items-center mt-[7%] px-20 ml-auto mr-auto gap-40'>
+    <div className=''>
+    <h1 className="font-bold text-3xl text-left mt-8 bg-gradient-to-r from-purple-600 to-pink-500 text-transparent bg-clip-text">Minimum Complexity</h1>
+      <h1 className='font-bold text-3xl ml-40 text-left bg-gradient-to-r from-purple-600 to-pink-500 text-transparent bg-clip-text'> Maximum Security</h1>
+      <p className='text-left'>Your custom app, meticulously tailored to your language preference, fortified with robust security measures, and primed for seamless deployment in the cloud.</p>
     </div>
     <div className=''>
-      <img src={photo} alt="" className='w-72 ' /></div>
+      <img src={photo} alt="" className='w-full ' /></div>
    </div>
-
+   <div className="flex justify-between mt-[4%] px-20">
+  <img src="/public/img/golang-1 (1).svg" alt="Description 1" className='w-20' />
+  <img src="/public/img/dot-net-core-7.svg" alt="Description 1" className='w-20' />
+  <img src="/public/img/java-4 (2).svg" alt="Description 1" className='w-20' />
+  <img src="/public/img/nodejs (1).svg" alt="Description 1" className='w-20' />
+  <img src="/public/img/python-4.svg" alt="Description 1" className='w-20' />
+ 
+</div>
+</div>
    
    </>
   )
